ebc0ce151210223cf597b1dd62f959142f502884
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2021-12-03 08:45:27 +0100

    Make new dir if it does note exist when fetching input
    Also, prefix days with 0

--- input.sh
+++ input.sh
@@ -4,7 +4,9 @@ DAY=$1
 DIR=day$(printf "%02d" ${DAY})
 AOCCOOKIE=$(cat .session)
 
+mkdir -p $DIR
+
 curl \
-  -fSL -o ./day${DAY}/input.txt \
+  -fSL -o ${DIR}/input.txt \
   -H "Cookie: session=${AOCCOOKIE}" \
   https://adventofcode.com/2021/day/${DAY}/input
